83e3496e388d6e1fbebb8254b4228439095b7055
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-13 19:10:51 -0700

    Implement library list and hook up the redux store

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
-const App = () => {
-  return (
-    <Provider store={createStore(reducers)}>
-      <View>
-        <Header headerText="Tech Stack" />
-      </View>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={createStore(reducers)}>
+    <View>
+      <Header headerText="Tech Stack" />
+      <LibraryList />
+    </View>
+  </Provider>
+);
 
 export default App;
