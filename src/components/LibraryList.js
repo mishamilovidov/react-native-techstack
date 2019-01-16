3e46b429587865e7535d741518817c2130bf3bd9
Misha Milovidov <m.alexeyvich@gmail.com>
2019-01-16 10:14:42 -0700

    Add topics and descriptions

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,15 +1,24 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+  renderItem(library) {
+    return <ListItem library={library} />;
+  }
+
   render() {
-    console.log(this.props);
-    return;
+    return (
+      <FlatList
+        data={this.props.libraries}
+        renderItem={this.renderItem}
+        keyExtractor={(library) => library.id.toString()}
+      />
+    );
   }
 }
 
-const mapStateToProps = state => {
-  return { libraries: state.libraries };
-};
+const mapStateToProps = state => ({ libraries: state.libraries });
 
 export default connect(mapStateToProps)(LibraryList);
